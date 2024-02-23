86e0fa8947dbabf8bb7720b47b56ebcb0cb87a42
Max <s6503052412161@kmutnb.ac.th>
2024-02-23 09:51:35 +0700

    Update webborad a littal bit
    Update webborad a littal bit

--- php/src/nav.php
+++ php/src/nav.php
@@ -1,35 +1,64 @@
-<?php
-if (!isset($_SESSION['id'])) {
+<!DOCTYPE html>
+<html lang="en">
 
-?>
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php"><i class="bi bi-house-door-fill"></i>Home</a>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="login.php" style="color: #fff;"><i class="bi bi-pencil-square"></i>Login</a>
-                </li>
-            </ul>
-        </div>
-    </nav><?php } else { ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website With Login & Registration | Codehal</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<!-- Before Login -->
 
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php"><i class="bi bi-house-door-fill"></i>Home</a>
-            <ul class="navbar-nav">
-                <div class="dropdown">
-                    <a class="btn btn-outline-secondary dropdown-toggle btn-sm" type="button" id="Button1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="bi bi-person-lines-fill"></i>
-                        <?php echo $_SESSION['username']; ?>
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="Button1">
-                        <li><a href="logout.php" class="dropdown-item">
-                                <i class="bi bi-power">Logout</i>
-                            </a></li>
-                    </ul>
-                </div>
-            </ul>
-        </div>
-    </nav>
+<?php if (!isset($_SESSION['id'])) { ?>
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" >
+   <div class="container-fluid">
+    <a class="navbar-brand text-white" href="index.php">Welcome!</a>
+      <button class="navbar-toggler white-border" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon "></span>
+      </button>
+
+
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="#"><i class="bi bi-house-fill"></i>&nbsp;&nbsp; Dormitory</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-white"  aria-current="page" href="board.php"><i class="bi bi-chat-square-text-fill"></i>&nbsp;&nbsp; WebBoard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-white"  aria-current="page" href="login.php"><i class="bi bi-pencil-square"></i>&nbsp;&nbsp; Login</a>
+        </li>
+    </div>
+  </div>
+</nav>
+ 
+<?php } else { ?>
+<!-- After login  -->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-blur fixed-top " style="backdrop-filter: blur(30px);">
+   <div class="container-fluid">
+    <a class="navbar-brand text-white" href="index.php">Welcome!<?php echo $_SESSION['username']; ?></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="#"><i class="bi bi-house-fill"></i>&nbsp;&nbsp; Dormitory</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-white"  aria-current="page" href="board.php"><i class="bi bi-chat-square-text-fill"></i>&nbsp;&nbsp; WebBoard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active text-white"  aria-current="page" href="login.php"><i class="bi bi-pencil-square"></i>&nbsp;&nbsp; Logout</a>
+        </li>
+    </div>
+  </div>
+</nav>
 
 <?php } ?>
+
+</html> 
